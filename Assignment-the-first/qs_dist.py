fe136e98990a3e12a9a600efa099ece26e249f5e
Christina Zakarian <czakarian@github.com>
2021-07-30 16:29:40 -0700

    added python files

--- Assignment-the-first/qs_dist.py
+++ Assignment-the-first/qs_dist.py
@@ -21,14 +21,17 @@ plot = args.plot
 
 read_length = 0
 linecount = 0
-    
+
+# Determine the length of the sequence lines to be used for list initialization 
 with gzip.open(file, "rt") as fr:
     fr.readline()
     seqline = fr.readline()
     read_length = len(seqline)
 
+# initialize list to store ongoing sums
 my_list = [0] * (read_length-1)
 
+# parse the FASTQ file and sum the quality scores at each base pair
 with gzip.open(file, "rt") as fr:
     for line in fr:
         line = line.strip()     
@@ -42,6 +45,7 @@ for i, qv in enumerate(my_list):
     my_list[i] = (qv / (linecount/4))
     print(str(i) + "\t" + str(my_list[i]))
 
+# Generate plots of the quality score distribution
 x = range(len(my_list))
 y = my_list
 plt.plot(x, y)
