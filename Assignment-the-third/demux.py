d119e7375e7d14fb4b19dac02e46c63eaf8a9594
Christina Zakarian <czakarian@github.com>
2021-08-10 17:14:10 -0700

    update script

--- Assignment-the-third/demux.py
+++ Assignment-the-third/demux.py
@@ -15,17 +15,15 @@ def get_args():
     parser.add_argument("-f", "--files", help="Input FASTQ filenames (one read pair, one index pair)", nargs=4, required=True)
     parser.add_argument("-i", "--indexes", help="Index filename", required=True)
     parser.add_argument("-d", "--direct", help="Output directory", required=True)
+    parser.add_argument("-c", "--counts", help="Counts filename", required=True)
     return parser.parse_args()
-
-###
-# Making sure input files are zipped and FASTQ files? do a check?
-###
 
 # store the command line args in variables
 args = get_args()
 files= args.files
 indexes = args.indexes
 direct = args.direct
+countsfile = args.counts
 
 # Dictionary to store list of index seqs and other related index info (sample, group, treatment, index)
 # Key = index sequence, Value = Dictionary identifying sample, group, treatment, and index values
@@ -45,16 +43,11 @@ with open(indexes, "rt") as fr:
             index_info_dict['index'] = cols[3]
         index_dict[cols[4]] = index_info_dict
 
-#print(index_dict)
-
 # Dictionary to store indexes and their reverse complements // to avoid calling rev_comp for every index read while parsing index 2 fq file
 # Keys = Index, Value = Reverse Complement of Index 
 rc_index_dict = {}
 for i in index_dict:
     rc_index_dict[Bioinfo.rev_comp(i)]= i
-
-#print(index_dict)
-#print(rc_index_dict)
 
 # Dictionary to store counts for all matched indexes read-pairs
 # Key = matched index pair, Value = count
@@ -87,87 +80,66 @@ for i in index_dict:
     file_handlers_dict[r1] = gzip.open(direct + "/" + r1, "wt") 
     file_handlers_dict[r2] = gzip.open(direct + "/" + r2, "wt")
 
-
 # open input files (pair of read FASTQs and pair of index FASTQs)
 fq1 = gzip.open(files[0], "rt")
 fq2 = gzip.open(files[1], "rt")
 fq3 = gzip.open(files[2], "rt")
 fq4 = gzip.open(files[3], "rt")
 
-readcounter = 0
+
+
 
 # Begin parsing the 4 input files one read at a time in parallel
 while True:
-    readcounter +=1
-    if readcounter % 2500000 == 0:
-        print("Read ", readcounter)
-
-    r1_lines = [0] * 4
-    r2_lines = [0] * 4
-    i1_lines = [0] * 4
-    i2_lines = [0] * 4
+    # use a dict instead of list to store the 4 lines per each of the 4 reads??
+    fq_reads = {"r1":{1:"", 2:"", 3:"", 4:""}, "r2":{1:"", 2:"", 3:"", 4:""}, "i1":{1:"", 2:"", 3:"", 4:""}, "i2":{1:"", 2:"", 3:"", 4:""}}
     for i in range(4):
-        r1_lines[i] = fq1.readline().strip()
-        r2_lines[i] = fq4.readline().strip()
-        i1_lines[i] = fq2.readline().strip()
-        i2_lines[i] = fq3.readline().strip()
+        fq_reads["r1"][i] = fq1.readline().strip()
+        fq_reads["r2"][i] = fq4.readline().strip()
+        fq_reads["i1"][i] = fq2.readline().strip()
+        fq_reads["i2"][i] = fq3.readline().strip()  
     
     # exit when you reach end of the file
-    if r1_lines[i] == '':
+    if fq_reads["r1"][0] == '':
         break
 
-    # initialized specific variables for each of the 4 lines of the 4 files // just for easier reference 
-    index1_seq = i1_lines[1]
-    index2_seq = i2_lines[1]
-    index1_qs = i1_lines[3]
-    index2_qs = i2_lines[3]
-    index1_desc = i1_lines[2]
-    index2_desc = i2_lines[2]
-
-    read1_seq = r1_lines[1] 
-    read2_seq = r2_lines[1]
-    read1_qs = r1_lines[3]
-    read2_qs = r2_lines[3]
-    read1_desc = r1_lines[2]
-    read2_desc = r2_lines[2]
-
     # store the rc of index2 seq so you only need to call the actual fxn once
-    rc = Bioinfo.rev_comp(index2_seq)
+    rc = Bioinfo.rev_comp(fq_reads["i2"][1])
 
     # append i1 and i2 rc indexes to the header
-    header = r1_lines[0].split(' ')
-    header = header[0] + "-" + index1_seq + "-" + rc
+    header = fq_reads["r1"][0].split(' ')[0] + "-" + fq_reads["i1"][1] + "-" + rc
     
     # determine which bucket your read will be sent to
     bucket = ""
 
     # check if both indexes are valid / don't contain N's and meet the quality score cutoff
-    if index1_seq in index_dict and index2_seq in rc_index_dict and Bioinfo.meets_Qcutoff(index1_qs) and Bioinfo.meets_Qcutoff(index2_qs):
+    if fq_reads["i1"][1] in index_dict and fq_reads["i2"][1] in rc_index_dict and Bioinfo.meets_Qcutoff(fq_reads["i1"][3]) and Bioinfo.meets_Qcutoff(fq_reads["i2"][3]):
         # check if the 2 indexes are reverse comp of each other
-        if index1_seq == rc:
-            bucket = index_dict[index1_seq]["index"]
-            counter_matched_dict[index1_seq] += 1
+        if fq_reads["i1"][1] == rc:
+            bucket = index_dict[fq_reads["i1"][1]]["index"]
+            counter_matched_dict[fq_reads["i1"][1]] += 1
             counter_matched += 1
         else:
             bucket = "swapped"
-            s = index1_seq + "-" + index2_seq
-            if s in counter_swapped_dict:
-                counter_swapped_dict[s] += 1
+            counter_swapped += 1
+            i1i2 = fq_reads["i1"][1] + "-" + rc
+            i2i1 = rc + "-" + fq_reads["i1"][1]
+            if i1i2 in counter_swapped_dict:
+                counter_swapped_dict[i1i2] += 1
+            elif i2i1 in counter_swapped_dict:
+                counter_swapped_dict[i2i1] += 1
             else:
-                counter_swapped_dict[s] = 1
-                counter_swapped += 1
+                counter_swapped_dict[i1i2] = 1          
     else:
         bucket = "unknown"
         counter_unknown += 1
-
-    #print(bucket)
 
     # append each read to the corresponding bucket file
     r1file = file_handlers_dict[bucket + "_R1.fq.gz"]
     r2file = file_handlers_dict[bucket + "_R2.fq.gz"]
 
-    r1file.write(header + "\n" + read1_seq + "\n" + read1_desc + "\n" + read1_qs + "\n")
-    r2file.write(header + "\n" + read2_seq + "\n" + read2_desc + "\n" + read2_qs + "\n")
+    r1file.write(fq_reads["r1"][0] + "\n" + fq_reads["r1"][1] + "\n" + fq_reads["r1"][2] + "\n" + fq_reads["r1"][3] + "\n")
+    r2file.write(fq_reads["r2"][0] + "\n" + fq_reads["r2"][1] + "\n" + fq_reads["r2"][2] + "\n" + fq_reads["r2"][3] + "\n")
 
 # close the input files 
 fq1.close()
@@ -179,13 +151,24 @@ fq4.close()
 for file in file_handlers_dict:
     file_handlers_dict[file].close()
 
-with open("counts.txt", "w") as fw:
-    fw.write("Number of matched index-pairs: " + str(counter_matched) + "\n")
-    fw.write("Number of swapped index-pairs: " + str(counter_swapped) + "\n")
-    fw.write("Number of unknown index-pairs: " + str(counter_unknown) + "\n")
+# stats
+total_reads = counter_matched + counter_swapped + counter_unknown
+perc_matched = counter_matched / total_reads * 100
+perc_swapped = counter_swapped / total_reads * 100
+perc_unknown = counter_unknown / total_reads *100
+
+
+with open(countsfile, "w") as fw:
+    fw.write("Number (& %) of matched index-pairs: " + str(counter_matched) + " " + str(perc_matched) + "%\n")
+    fw.write("Number (& %) of swapped index-pairs: " + str(counter_swapped) + " " + str(perc_swapped) + "%\n")
+    fw.write("Number (& %) of unknown index-pairs: " + str(counter_unknown) + " " + str(perc_unknown) + "%\n")
     fw.write("Matched read-pairs: \n") 
     for i in counter_matched_dict:
         fw.write(i + "\t" + str(counter_matched_dict[i]) + "\n")
     fw.write("Swapped read-pairs: \n")
     for i in counter_swapped_dict:
         fw.write(i + "\t" + str(counter_swapped_dict[i]) + "\n") 
+    
+
+# percentage of reads with from each sample (A1 = #reads/ total reads)
+# percentage of index hopped, matching, and swapped
